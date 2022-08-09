c9be91e62d115e27171bdb7ca9566a1088e7c99e
Skynet006 <luckyezealor@gmail>
2022-08-09 13:32:12 -0700

    final checking

--- string_manipulators.c
+++ string_manipulators.c
@@ -86,8 +86,8 @@ char *rev_string(char *s)
 
 
 /**
- * * formated_string - Receives the main string and all the necessary parameters
- * * to print a formated string.
+ * * formated_string - Receives the main string and all the necessary
+ * * parameters to print a formated string.
  * * @format: A string containing all the desired characters.
  * * @f_list: A list of all the posible functions.
  * * @arg_list: A list containing all the argumentents passed to the program.
