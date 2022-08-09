a53c5388c7fb4297c118682068c69be1079dff17
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2022-08-09 22:51:17 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -41,13 +41,11 @@ int _printf(const char *format, ...)
 /**
  * * _putchar - writes the character c to stdout
  * * @c: The character to print
- * *
  * * Return: On success 1.
  * * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-
